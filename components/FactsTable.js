4486299bced248f094abb463defb8913b506d866
lg550055 <lg550055@hotmail.com>
2022-06-23 20:55:55 -0500

    Adds update form

--- components/FactsTable.js
+++ components/FactsTable.js
@@ -1,30 +1,64 @@
+import { useState } from "react"
 
 
 export default function FactsTable({ facts, onDelete, update }) {
 
+  const [display , setDisplay] = useState(false)
+  const [fact , setFact] = useState({})
+
+  function handleUpdate(fact) {
+    setFact(fact)
+    setDisplay(true)
+    }
+
+  function UpdateForm(fact) {
+    fact = fact.fact
+    return (
+      <form>
+        <legend>Fact Update Form</legend>
+        <input placeholder="fact" name="fact" defaultValue={fact.fact} />
+        <input type="date" name="date" defaultValue={fact.date} />
+        <select name='flag' >
+          <option value='c'>Civil Rights</option>
+          <option value='v'>Voting</option>
+          <option value='s'>Slavery</option>
+        </select>
+        <input placeholder="source" name="source" defaultValue={fact.source} />
+        <label >verified</label>
+        <input type='checkbox' name="verified" defaultValue={fact.verified} />
+        <label >progress</label>
+        <input type='checkbox' name="progress" defaultValue={fact.progress}/>
+        <button>Update</button>
+      </form>
+    )
+  }
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Fact</th>
-          <th>Date</th>
-          <th>Category</th>
-          <th>Source</th>
-          <th>Verified</th>
-        </tr>
-      </thead>
-      <tbody>
-        {facts.map(fact => (
-          <tr key={fact.id}>
-            <td>{fact.fact}</td>
-            <td>{fact.date}</td>
-            <td>{fact.flags}</td>
-            <td><a href={fact.source}>{fact.source}</a></td>
-            <td>{fact.verified ? 'T' : 'F'}</td>
-            <td><button onClick={() => onDelete(fact.id)}>Delete</button><button onClick={update}>Update</button></td>
+    <>
+      {display && <UpdateForm fact={fact}/>}
+      <table>
+        <thead>
+          <tr>
+            <th>Fact</th>
+            <th>Date</th>
+            <th>Category</th>
+            <th>Source</th>
+            <th>Verified</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {facts.filter(fact => !fact.verified).map(fact => (
+            <tr key={fact.id}>
+              <td>{fact.fact}</td>
+              <td>{fact.date}</td>
+              <td>{fact.flags}</td>
+              <td><a href={fact.source}>{fact.source}</a></td>
+              <td>{fact.verified ? 'T' : 'F'}</td>
+              <td><button onClick={()=>onDelete(fact.id)}>Delete</button><button onClick={()=>handleUpdate(fact)}>Update</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
